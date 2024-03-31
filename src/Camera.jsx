a29dc69943e4023dedecd38aa827a266433e7ac0
franco22bellido <franco.bellido22@gmail.com>
2024-03-31 14:51:43 -0300

    add bootstrap style

--- src/Camera.jsx
+++ src/Camera.jsx
@@ -7,29 +7,31 @@ import { useEffect } from 'react';
 const Camera = () => {
     const constraints = {
         video: {
-          width: {
-            min: 1280,
-            ideal: 1920,
-            max: 2560,
-          },
-          height: {
-            min: 720,
-            ideal: 1080,
-            max: 1440
-          },
-          facingMode: 'environment'
+            width: {
+                min: 1280,
+                ideal: 1920,
+                max: 2560,
+            },
+            height: {
+                min: 720,
+                ideal: 1080,
+                max: 1440
+            },
+            facingMode: 'environment'
         }
-      };
+    };
 
     const [videoProperties, setVideoProperties] = useState();
     const [qrDecoded, setQrDecoded] = useState("no info");
 
-    const openCamera = async ()=> {
-        if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
+    const openCamera = async () => {
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
             try {
-                const stream = await navigator.mediaDevices.getUserMedia({video: {
-                    facingMode : 'environment'
-                }});
+                const stream = await navigator.mediaDevices.getUserMedia({
+                    video: {
+                        facingMode: 'environment'
+                    }
+                });
                 // videoProperties.srcObject = stream;
                 // videoProperties.play();
                 let video = document.getElementById('video');
@@ -50,11 +52,17 @@ const Camera = () => {
     }
 
     return (
-        <div>
-          <video id='video' autoPlay ref={video => {setVideoProperties(video)}} ></video>
-          <h1>{qrDecoded}</h1>
-          <button onClick={()=> openCamera() } className="btn btn-success">open camera</button>
-        </div>
+        <section className='container'>
+            <div className='row'>
+                <div className='card col-md-8 mx-auto'>
+                    <video className='card-img-top mx-auto mt-3' id='video' autoPlay ref={video => { setVideoProperties(video) }} ></video>
+                    <div className='card-body'>
+                        <h5 className='card-title'>{qrDecoded}</h5>
+                        <button onClick={() => openCamera()} className="btn btn-success">Scan Qr</button>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
